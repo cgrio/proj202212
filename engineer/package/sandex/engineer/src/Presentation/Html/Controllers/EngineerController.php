044743d06bcc03132853545a66b9d8a1776f16fc
Alessandro Correia <alessrio@gmail.com>
2022-12-26 05:45:57 -0300

    retirado codigo exemplo

--- engineer/package/sandex/engineer/src/Presentation/Html/Controllers/EngineerController.php
+++ engineer/package/sandex/engineer/src/Presentation/Html/Controllers/EngineerController.php
@@ -177,25 +177,6 @@ class EngineerController extends Controller
         return $results;
     }
 
-
-    private function groupByOwners(array $files) : array
-    {
-
-        $result = [];
-            foreach ($files as $key=>$value) {
-                $result[$value][] = $key;
-            }
-            return $result;
-    }
-
-        // $files = array
-        // (
-        //     "Input.txt" => "Randy",
-        //     "Code.py" => "Stan",
-        //     "Output.txt" => "Randy"
-        // );
-        // var_dump(groupByOwners($files));
-
 }
 
 
