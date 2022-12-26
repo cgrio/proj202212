466b577275bde182a07e93ae9772fdc6e313996a
Alessandro Correia <alessrio@gmail.com>
2022-12-26 05:44:11 -0300

    Adicionando segunda fase

--- engineer/package/sandex/engineer/src/Presentation/Html/views/engineer/editor.blade.php
+++ engineer/package/sandex/engineer/src/Presentation/Html/views/engineer/editor.blade.php
@@ -4,12 +4,7 @@
 
 
 	<div class="row justify-content-center">
-		@include('sandex.engineer.engineer.partes.layouts.tabs_verticais.container',[
-		'label'=> 'label',
-		'id'=> 'id',
-		'active'=> 'active',
-		'selected_boolean'=> false,
-		])
+		@include('sandex.engineer.engineer.partes.layouts.tabs_verticais.container')
 	</div>
 </div>
 @endsection
