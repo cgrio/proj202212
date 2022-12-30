8a24e14d02faf29198368c6bf25c6129b2a1d04d
Alessandro Correia <alessrio@gmail.com>
2022-12-29 21:19:16 -0300

    javascript dos campos

--- engineer/resources/views/sandex/engineer/engineer/editor.blade.php
+++ engineer/resources/views/sandex/engineer/engineer/editor.blade.php
@@ -26,41 +26,73 @@
     </form>
     </div>
 </div>
+<div id="app" class="app"></div>
+<div id="main" class="main"></div>
 @endsection
 
 @section('scripts')
 <script type="text/javascript">
-
-    function saidaDeCampo(){
+    const camelCase = str => {
+    let string = str.toLowerCase().replace(/[^A-Za-z0-9]/g, ' ').split(' ')
+                    .reduce((result, word) => result + capitalize(word.toLowerCase()))
+    return string.charAt(0).toLowerCase() + string.slice(1)
     }
-//inputs as NodeList
-var inputs = document.querySelectorAll("input");
 
+    const capitalize = str => str.charAt(0).toUpperCase() + str.toLowerCase().slice(1)
+    const firstUpper = str => str.charAt(0).toUpperCase() + str.slice(1)
+    const toSnakeCase = str =>
+    str &&
+    str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map(x => x.toLowerCase())
+    .join('_');
+
+    const snakeToCamel = str =>  str.toLowerCase().replace(/([-_][a-z])/g, group =>
+            group
+            .toUpperCase()
+            .replace('-', '')
+            .replace('_', '')
+        );
+
+
+const camposDuplos = [
+    'fornecedor',
+    'pacote',
+    'modelosingular',
+    'modeloplural'
+];
+//inputs as NodeList
+let inputs = document.querySelectorAll("input");
 // Loop to asign event
 inputs.forEach(function(item){
-    item.addEventListener("blur", function(gatilho){
-
-       // console.log(gatilho.getAttribute('name'));
-        console.log(gatilho);
-       let campo = gatilho.target;
-       console.log(campo.getAttribute('name'));
-       let listaInputs = document.querySelectorAll("input");
-       listaInputs.forEach(function(itemLista){
-        let regex = /[^a-z]/gi;
-            if(itemLista.getAttribute('name') == campo.getAttribute('name')){
-                console.log('sim');
-            }else{
-                if(itemLista.getAttribute('name').replace(regex, "").toLowerCase() == campo.getAttribute('name').replace(regex, "").toLowerCase()){
-                console.log('encontrei o igual');
-                console.log(/[A-Z]/.test(itemLista.getAttribute('name').replace(regex, "")));
-            }
-
-        console.log('nao');
-}
-       });
-
+    item.addEventListener('blur', function(e){
+        regrasFormulario(e.target);
     });
-   });
+                    });
+
+function regrasFormulario(campo){
+
+        inputs.forEach(function(itemLista){
+            let regex = /[^campo]/gi;
+            regex = /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g;
+            regex = /[^a-zA-Z]/gi;
+            //if(itemLista.getAttribute('name').replace(regex, "").toLowerCase() == campo){
+                if(itemLista.getAttribute('name').replace(regex, "").toLowerCase() === campo.getAttribute('name').replace(regex, "").toLowerCase()){
+                console.log('encontrei com mesmo nome lower');
+                   if(itemLista.getAttribute('name').replace(regex, "") !== campo.getAttribute('name').replace(regex, "")){
+                    console.log("Mesmo campo mas com case diferente");
+                    console.log(itemLista.getAttribute('name'));
+                    if((/[A-Z]/).test(itemLista.getAttribute('name'))){
+                        itemLista.value = firstUpper(snakeToCamel(campo.value));
+                    }else{
+                        itemLista.value = toSnakeCase(campo.value);
+                    }
+                   }
+
+            }
+    });
+
+}
 
     </script>
 @endsection
