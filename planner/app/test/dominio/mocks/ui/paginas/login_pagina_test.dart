bf497b6a7bf10b0cc89c1b2cd1b015dae98f7af3
Alessandro Silva <alessrio@gmail.com>
2023-04-18 05:46:25 -0300

    movendo a inicialização da página do teste

--- planner/app/test/dominio/mocks/ui/paginas/login_pagina_test.dart
+++ planner/app/test/dominio/mocks/ui/paginas/login_pagina_test.dart
@@ -6,9 +6,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 
 void main() {
-  testWidgets('Deve carregar com o estado inicial', (WidgetTester tester) async {
+  Future<void> carregarPagina(WidgetTester tester) async {
     final loginPagina = MaterialApp(home: LoginPagina());
     await tester.pumpWidget(loginPagina);
+  }
+
+  testWidgets('Deve carregar com o estado inicial', (WidgetTester tester) async {
+    await carregarPagina(tester);
     final campoEmailFilhos = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
     expect(campoEmailFilhos, findsOneWidget, reason: "Procura apenas um campo com label Email");
     expect(campoEmailFilhos.toString().indexOf('errorText'), isNegative, reason: "Verifico se os filhos não existem errorText");
@@ -23,5 +27,13 @@ void main() {
     final botaoLogin = tester.widget<TextButton>(find.byType(TextButton));
 
     expect(botaoLogin.onPressed, isNotNull);
+  });
+
+  testWidgets('Testar validador de página', (WidgetTester tester) async {
+    await carregarPagina(tester);
+    final campoEmailFilhos = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(campoEmailFilhos, findsOneWidget, reason: "Procura apenas um campo com label Email");
+    expect(campoEmailFilhos.toString().indexOf('errorText'), isNegative, reason: "Verifico se os filhos não existem errorText");
+    expect(campoEmailFilhos.toString().indexOf('textAlign'), isNonZero, reason: "Verifico se tem a propriedade textAlign");
   });
 }
